5ec5977edbbdb89231efae4f065c1550139ae7d9
joaquim manhica <joaquimmanhica6@gmail.com>
2023-07-11 12:46:07 -0700

    commit inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,189 @@
+import React from "react";
 import Image from 'next/image'
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { FaPlug } from 'react-icons/fa';
+import { IoIosPulse } from 'react-icons/io';
+import { GiEyelashes } from 'react-icons/gi';
+import { BiBook } from 'react-icons/bi';
+import {
+  AiFillLinkedin,
+  AiFillYoutube,
+  AiFillGithub,
+} from "react-icons/ai";
+
+
+import deved from "../../public/dev-ed-wave.png";
+import code from "../../public/code.png";
+import design from "../../public/design.png";
+import consulting from "../../public/consulting.png";
+import web1 from "../../public/web1.png";
+import web2 from "../../public/web2.png";
+import web3 from "../../public/web3.png";
+import web4 from "../../public/web4.png";
+import web5 from "../../public/web5.png";
+import web6 from "../../public/web6.png";
+import en_ment from "../../public/ensin-ment.png";
 
 export default function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+   
+    <main className='bg-white px-10 font-poppins'>
+      <nav className='flex py-10 mb-12 justify-between sticky top-0 bg-white'>
+          <h1 className='text-xl font-poppins'>
+            J . Manhica
+          </h1>
+          <ul className='flex items-center'>
+            <li><BsFillMoonStarsFill className='cursor-pointer text-2xl'/></li>
+            <li> <a className=' bg-gray-800 text-white px-8 py-2 rounded-md ml-3' href='#'>Curriculo</a></li>
+          </ul>
+        </nav>
+      <section className='min-h-screen bg-white'>
+        <div className='relative bg-gradient-to-t from-gray-950 rounded-full mx-auto w-40 h-40 overflow-hidden'>
+         <GiEyelashes size={100}/>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <div className='text-center p-10'>
+          <h2 className='text-5xl py-2 text-gray-800 font-medium'>Joaquim Manhica</h2>
+          <h3 className='text-2xl py-2'>Especialista em desenvolvimento Mobile</h3>
+          <p className='text-gray-500 py-5 leading-8'>
+          Como engenheiro de software com formação na universidade pedagógica de maputo, 
+          tenho um histórico comprovado de sucesso e conquistas significativas. 
+          Com experiência em desenvolvimento de aplicativos nativos e híbridos,
+           estou entusiasmado em destacar minhas habilidades e contribuições para sua equipe.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        </div>
+        <div className='text-5xl flex justify-center gap-16 py-3'>
+          <AiFillLinkedin/>
+          <AiFillGithub/>
+          <AiFillYoutube/>
+        </div>
+       
+      </section>
+      <section>
+          <div className='text-center mt-10'>
+            <h3 className="text-3xl py-1 dark:text-white ">Meus serviços</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              Sou um desenvolvedor Fullstack JavaScript especializado em desenvolvimento mobile e ensino de programação mobile. Ofereço serviços completos de criação de aplicativos móveis nativos para iOS e Android, utilizando tecnologias como React Native e Node.js. Também forneço treinamentos e mentorias personalizadas para aqueles que desejam aprender a programar aplicativos móveis. Minha abordagem é centrada no cliente, priorizando a usabilidade, experiência do usuário e performance. Tenho experiência em todas as etapas do ciclo de desenvolvimento e garanto resultados de alta qualidade.
+              
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+               Aprender programação mobile pode ser desafiador, mas estou aqui para ajudar você a dominar as habilidades necessárias.
+            </p>
+          </div>
+          <div className="lg:flex gap-10">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
+            <div className='flex justify-center align-middle'>
+            <FaPlug size={50}/>
+            </div>
+              <h3 className="text-lg font-medium pt-8 pb-2  ">
+                Aplicativos hibridos
+              </h3>
+              <p className="py-2">
+              Aplicativos híbridos são aplicações que combinam elementos de aplicativos nativos e web, sendo desenvolvidos com tecnologias web (HTML, CSS, JavaScript) e encapsulados em um contêiner nativo para funcionar em diferentes plataformas móveis.
+              </p>
+              <h4 className="py-4 text-gray-900 font-bold">Ferramentas usadas no desenvolvimento</h4>
+              <p className="text-gray-800 py-1">React Native</p>
+              <p className="text-gray-800 py-1">Ionic</p>
+              <p className="text-gray-800 py-1">Flutter</p>
+              <p className="text-gray-800 py-1">PhoneGap/Cordova</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+            <div className='flex justify-center align-middle'>
+            <IoIosPulse size={50}/>
+            </div>
+              <h3 className="text-lg font-medium pt-8 pb-2 ">
+                Aplicativos nativos
+              </h3>
+              <p className="py-2">
+              Um aplicativo nativo é um software desenvolvido para uma plataforma específica, como iOS ou Android, usando linguagens de programação nativas suportadas pela plataforma. Eles oferecem acesso total aos recursos do dispositivo e proporcionam uma experiência de usuário otimizada e integrada.
+              </p>
+              <h4 className="py-4 text-gray-900 font-bold">Ferramentas usadas no desenvolvimento</h4>
+              
+              <p className="text-gray-800 py-1">Xcode</p>
+              <p className="text-gray-800 py-1">Android Studio</p>
+              <p className="text-gray-800 py-1">Visual Studio</p>
+              <p className="text-gray-800 py-1">Flutter</p>
+              <p className="text-gray-800 py-1">React Native</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+            <div className='flex justify-center align-middle'>
+              <BiBook size={50}/>
+            </div>
+              <h3 className="text-lg font-medium pt-8 pb-2 ">Ensino e mentoria</h3>
+              <p className="py-2">
+              Impulsione sua carreira no desenvolvimento mobile com mentoria especializada! Aproveite a oportunidade de aprender com um desenvolvedor experiente e aprofunde seus conhecimentos em iOS, Android ou desenvolvimento multiplataforma. Junte-se a mim e alcance novos patamares no mundo mobile. Invista no seu futuro agora!
+              </p>
+              <div className='flex justify-center align-middle'>
+              <Image
+              alt='profile-pic'
+                className="rounded-lg object-cover"
+                sizes='100%'
+                src={en_ment}
+              />
+              </div>
+              
+            </div>
+          </div>
+        </section>
+        <section className="text-center py-10">
+          <div>
+            <h3 className="text-3xl py-1 dark:text-white ">Meus projectos</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+            Explore o poder da mentoria personalizada no desenvolvimento mobile com um especialista comprovado. Aumente sua expertise em projetos mobile, domine tecnologias avançadas e acelere sua carreira. Receba orientação direta e insights valiosos para criar aplicativos de sucesso. Invista em seu crescimento profissional e desbloqueie todo o potencial dos seus projetos mobile.
+            </p>
+          </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1 ">
+              <Image
+              alt='profile-pic'
+                className="rounded-lg object-cover"
+                sizes='100%'
+               
+                src={web1}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+              alt='profile-pic'
+                className="rounded-lg object-cover"
+                sizes='100%'
+                src={web2}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+              alt='profile-pic'
+                className="rounded-lg object-cover"
+                sizes='100%'
+                src={web3}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+              alt='profile-pic'
+                className="rounded-lg object-cover"
+                sizes='100%'
+                src={web4}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+              alt='profile-pic'
+                className="rounded-lg object-cover"
+                sizes='100%'
+                src={web5}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+              alt='profile-pic'
+                className="rounded-lg object-cover"
+                sizes='100%'
+                src={web6}
+              />
+            </div>
+          </div>
+        </section>
     </main>
   )
 }
